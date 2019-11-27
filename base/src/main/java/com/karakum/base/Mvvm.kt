48b779fe1de1dfcd0d1b71519d4e4690844adcca
Kyle Amyx <kyle.amyx@duethealth.com>
2019-11-27 14:36:15 -0500

    base lib added

---
+++ base/src/main/java/com/karakum/base/Mvvm.kt
@@ -0,0 +1,13 @@
+package com.karakum.base
+
+import android.os.Parcelable
+
+
+/**
+ * State interface which will be a generic of my [BaseViewModel]
+ */
+interface Mvvm {
+
+    interface State : Parcelable
+
+}
